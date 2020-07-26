b8de189bcbd5fc4bc0547f62ae36a417aceb2f61
JahzielCabrera <jahziel_oswaldo@hotmail.com>
2020-07-26 02:49:41 -0500

    Error de Handlebars solucionado

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 
+const Handlebars = require('handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+
 // Initializations 
 const app = express();
 
@@ -14,7 +17,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
+    extname: '.hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
 }));
 app.set('view engine', '.hbs');
 
